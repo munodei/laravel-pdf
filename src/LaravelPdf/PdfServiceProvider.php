8aa904d1daafa49036c82532302644fd8c5c996e
munodei <terrebcemunodei25@gmail.com>
2024-08-23 10:55:29 +0200

    Updated package for Laravel 11 compatibility and added tests

--- src/LaravelPdf/PdfServiceProvider.php
+++ src/LaravelPdf/PdfServiceProvider.php
@@ -6,11 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class PdfServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->app->singleton('mpdf.wrapper', function ($app) {
@@ -20,19 +15,10 @@ class PdfServiceProvider extends ServiceProvider
         $this->app->alias('mpdf.wrapper', 'MPDF');
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        // Publish the configuration file
         $this->publishes([
             __DIR__.'/config/pdf.php' => config_path('pdf.php'),
         ]);
-
-        // Laravel 11: Automatically register the service provider
-        ServiceProvider::addProviderToBootstrapFile(self::class);
     }
 }
