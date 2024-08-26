aa6e0d74e1a8e5155ba964bacf704cbeb087c571
munodei <terrebcemunodei25@gmail.com>
2024-08-26 17:14:09 +0200

    LaravelPdfWrapper loadView Fix

--- src/LaravelPdfWrapper.php
+++ src/LaravelPdfWrapper.php
@@ -1,17 +1,22 @@
 <?php
 
+
 namespace Niklasravnsborg\LaravelPdf;
 
+
 use Mpdf\Mpdf;
+
 
 class LaravelPdfWrapper
 {
     protected $mpdf;
 
+
     public function __construct()
     {
         $this->mpdf = new Mpdf();
     }
+
 
     public function loadHTML($html)
     {
@@ -19,14 +24,25 @@ class LaravelPdfWrapper
         return $this;
     }
 
+
     public function loadFile($file)
     {
         $html = file_get_contents($file);
         return $this->loadHTML($html);
     }
 
+
     public function output()
     {
         return $this->mpdf->Output('', 'S');
     }
+
+
+    public function loadView($view, $data = [], $mergeData = [])
+    {
+        $html = view($view, $data, $mergeData)->render();
+        return $this->loadHTML($html);
+    }
+
+
 }
